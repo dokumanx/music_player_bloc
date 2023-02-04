938b33efb39ce84e68ab700c470ef2fec7979059
Ferda Nahit Fidancı <fnfidanci@gmail.com>
2023-02-04 05:00:19 +0300

    refactor: update tests

--- test/music_player_bloc_test.dart
+++ test/music_player_bloc_test.dart
@@ -17,18 +17,25 @@ void main() {
   late MockPlaylistRepository mockRepository;
   late MockTrackTimer trackTimer;
   late MusicPlayerBloc musicPlayerBloc;
+  late MusicPlayerState state;
 
   setUp(() {
     mockRepository = MockPlaylistRepository();
     trackTimer = MockTrackTimer();
 
     when(mockRepository.getPlaylist()).thenAnswer(
-      (_) => blues90s,
+      (_) async => blues90s,
     );
 
     musicPlayerBloc = MusicPlayerBloc(
       playlistRepository: mockRepository,
       trackTimer: trackTimer,
+    );
+
+    state = const MusicPlayerState(
+      playlist: blues90s,
+      currentTime: Duration.zero,
+      currentTrackIndex: 0,
     );
   });
 
@@ -41,11 +48,13 @@ void main() {
             .thenAnswer((_) => const Duration(seconds: 0));
       },
       act: (bloc) => bloc.add(const MusicPlayerEvent.play()),
+      skip: 1,
       expect: () => <MusicPlayerState>[
-        MusicPlayerState.playing(
-          track: blues90s.tracks.first,
-          currentTime: Duration.zero,
-          currentTrackIndex: 0,
+        state.copyWith(
+          status: PlayerStatus.loading,
+        ),
+        state.copyWith(
+          status: PlayerStatus.playing,
         ),
       ],
     );
@@ -57,18 +66,16 @@ void main() {
         when(trackTimer.passedTime(trackDuration: anyNamed('trackDuration')))
             .thenAnswer((_) => const Duration(seconds: 15));
       },
-      seed: () => MusicPlayerState.playing(
-        track: blues90s.tracks.first,
+      seed: () => state.copyWith(
+        status: PlayerStatus.playing,
         currentTime: const Duration(seconds: 15),
-        currentTrackIndex: 0,
       ),
       act: (bloc) => bloc.add(const MusicPlayerEvent.pause()),
       expect: () => <MusicPlayerState>[
-        MusicPlayerState.paused(
-          track: blues90s.tracks.first,
-          lastTime: const Duration(seconds: 15),
-          currentTrackIndex: 0,
-        )
+        state.copyWith(
+          status: PlayerStatus.paused,
+          currentTime: const Duration(seconds: 15),
+        ),
       ],
     );
 
@@ -79,17 +86,15 @@ void main() {
         when(trackTimer.passedTime(trackDuration: anyNamed('trackDuration')))
             .thenAnswer((_) => const Duration(seconds: 15));
       },
-      seed: () => MusicPlayerState.paused(
-        track: blues90s.tracks.first,
-        lastTime: const Duration(seconds: 15),
-        currentTrackIndex: 0,
+      seed: () => state.copyWith(
+        status: PlayerStatus.paused,
+        currentTime: const Duration(seconds: 15),
       ),
       act: (bloc) => bloc.add(const MusicPlayerEvent.resume()),
       expect: () => <MusicPlayerState>[
-        MusicPlayerState.playing(
-          track: blues90s.tracks.first,
+        state.copyWith(
+          status: PlayerStatus.playing,
           currentTime: const Duration(seconds: 15),
-          currentTrackIndex: 0,
         ),
       ],
     );
@@ -99,21 +104,21 @@ void main() {
       build: () => musicPlayerBloc,
       setUp: () {
         when(trackTimer.passedTime(trackDuration: anyNamed('trackDuration')))
-            .thenAnswer((_) => const Duration(seconds: 10));
+            .thenAnswer((_) => const Duration(seconds: 0));
       },
-      seed: () => MusicPlayerState.playing(
-        track: blues90s.tracks.first,
-        currentTime: const Duration(seconds: 10),
-        currentTrackIndex: 0,
+      seed: () => state.copyWith(
+        status: PlayerStatus.playing,
       ),
       act: (bloc) => bloc.add(const MusicPlayerEvent.next()),
       wait: const Duration(milliseconds: 200),
       expect: () => <MusicPlayerState>[
-        const MusicPlayerState.loading(),
-        MusicPlayerState.playing(
-          track: blues90s.tracks[1],
-          currentTime: const Duration(seconds: 10),
+        state.copyWith(
+          status: PlayerStatus.loading,
+        ),
+        state.copyWith(
+          status: PlayerStatus.playing,
           currentTrackIndex: 1,
+          currentTime: const Duration(seconds: 0),
         ),
       ],
     );
@@ -121,15 +126,22 @@ void main() {
     blocTest(
       'Skip to the next track while there is no remaining track',
       build: () => musicPlayerBloc,
-      seed: () => MusicPlayerState.playing(
-        track: blues90s.tracks.last,
+      setUp: () {
+        when(trackTimer.passedTime(trackDuration: anyNamed('trackDuration')))
+            .thenAnswer((_) => const Duration(seconds: 0));
+      },
+      seed: () => state.copyWith(
+        status: PlayerStatus.playing,
+        currentTrackIndex: blues90s.tracks.length - 1,
         currentTime:
             trackTimer.passedTime(trackDuration: blues90s.tracks.last.duration),
-        currentTrackIndex: blues90s.tracks.length - 1,
       ),
       act: (bloc) => bloc.add(const MusicPlayerEvent.next()),
       expect: () => <MusicPlayerState>[
-        const MusicPlayerState.stopped(),
+        state.copyWith(
+          status: PlayerStatus.stopped,
+          currentTrackIndex: blues90s.tracks.length - 1,
+        ),
       ],
     );
 
@@ -140,18 +152,22 @@ void main() {
         when(trackTimer.passedTime(trackDuration: anyNamed('trackDuration')))
             .thenAnswer((_) => Duration.zero);
       },
-      seed: () => MusicPlayerState.playing(
-        track: blues90s.tracks[5],
-        currentTime: const Duration(seconds: 0),
+      seed: () => state.copyWith(
+        status: PlayerStatus.playing,
+        currentTime: const Duration(seconds: 10),
         currentTrackIndex: 5,
       ),
       act: (bloc) => bloc.add(const MusicPlayerEvent.previous()),
       wait: const Duration(milliseconds: 200),
       expect: () => <MusicPlayerState>[
-        const MusicPlayerState.loading(),
-        MusicPlayerState.playing(
-          track: blues90s.tracks[4],
-          currentTime: Duration.zero,
+        state.copyWith(
+          status: PlayerStatus.loading,
+          currentTime: const Duration(seconds: 10),
+          currentTrackIndex: 5,
+        ),
+        state.copyWith(
+          status: PlayerStatus.playing,
+          currentTime: const Duration(seconds: 0),
           currentTrackIndex: 4,
         ),
       ],
@@ -164,11 +180,7 @@ void main() {
         when(trackTimer.passedTime(trackDuration: anyNamed('trackDuration')))
             .thenAnswer((_) => Duration.zero);
       },
-      seed: () => MusicPlayerState.playing(
-        track: blues90s.tracks.first,
-        currentTime: Duration.zero,
-        currentTrackIndex: 0,
-      ),
+      seed: () => state.copyWith(status: PlayerStatus.playing),
       act: (bloc) => bloc.add(const MusicPlayerEvent.previous()),
       // Because it emits the same state, we don't expect anything
       expect: () => <MusicPlayerState>[],
--- test/music_player_bloc_test.mocks.dart
+++ test/music_player_bloc_test.mocks.dart
@@ -3,11 +3,11 @@
 // Do not manually edit this file.
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'dart:async' as _i5;
+import 'dart:async' as _i4;
 
 import 'package:mockito/mockito.dart' as _i1;
 import 'package:music_player_bloc/features/play_music/core/track_timer.dart'
-    as _i4;
+    as _i5;
 import 'package:music_player_bloc/features/play_music/models/playlist.dart'
     as _i2;
 import 'package:music_player_bloc/features/play_music/repository/playlist_repository.dart'
@@ -50,32 +50,33 @@ class _FakeDuration_1 extends _i1.SmartFake implements Duration {
 class MockPlaylistRepository extends _i1.Mock
     implements _i3.IPlaylistRepository {
   @override
-  _i2.Playlist getPlaylist() => (super.noSuchMethod(
+  _i4.Future<_i2.Playlist> getPlaylist() => (super.noSuchMethod(
         Invocation.method(
           #getPlaylist,
           [],
         ),
-        returnValue: _FakePlaylist_0(
+        returnValue: _i4.Future<_i2.Playlist>.value(_FakePlaylist_0(
           this,
           Invocation.method(
             #getPlaylist,
             [],
           ),
-        ),
-        returnValueForMissingStub: _FakePlaylist_0(
+        )),
+        returnValueForMissingStub:
+            _i4.Future<_i2.Playlist>.value(_FakePlaylist_0(
           this,
           Invocation.method(
             #getPlaylist,
             [],
           ),
-        ),
-      ) as _i2.Playlist);
+        )),
+      ) as _i4.Future<_i2.Playlist>);
 }
 
 /// A class which mocks [TrackTimer].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockTrackTimer extends _i1.Mock implements _i4.TrackTimer {
+class MockTrackTimer extends _i1.Mock implements _i5.TrackTimer {
   @override
   void dispose() => super.noSuchMethod(
         Invocation.method(
@@ -121,14 +122,14 @@ class MockTrackTimer extends _i1.Mock implements _i4.TrackTimer {
   Duration passedTime({required Duration? trackDuration}) =>
       (super.noSuchMethod(
         Invocation.method(
-          #remainingTime,
+          #passedTime,
           [],
           {#trackDuration: trackDuration},
         ),
         returnValue: _FakeDuration_1(
           this,
           Invocation.method(
-            #remainingTime,
+            #passedTime,
             [],
             {#trackDuration: trackDuration},
           ),
@@ -136,21 +137,21 @@ class MockTrackTimer extends _i1.Mock implements _i4.TrackTimer {
         returnValueForMissingStub: _FakeDuration_1(
           this,
           Invocation.method(
-            #remainingTime,
+            #passedTime,
             [],
             {#trackDuration: trackDuration},
           ),
         ),
       ) as Duration);
   @override
-  _i5.Stream<Duration> passedTimeStream({required Duration? trackDuration}) =>
+  _i4.Stream<Duration> passedTimeStream({required Duration? trackDuration}) =>
       (super.noSuchMethod(
         Invocation.method(
-          #remainingTimeStream,
+          #passedTimeStream,
           [],
           {#trackDuration: trackDuration},
         ),
-        returnValue: _i5.Stream<Duration>.empty(),
-        returnValueForMissingStub: _i5.Stream<Duration>.empty(),
-      ) as _i5.Stream<Duration>);
+        returnValue: _i4.Stream<Duration>.empty(),
+        returnValueForMissingStub: _i4.Stream<Duration>.empty(),
+      ) as _i4.Stream<Duration>);
 }
